669661b6d204b67a9525817cb35235a24cbb7642
Jaime Perez <jaipecas89@gmail.com>
2020-10-14 11:50:02 +0200

    Prog1 terminado

---
+++ Prog1.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Escriba un número: "))
+numero2 = int(input("Escriba un segundo número: "))
+
+media = (numero1 + numero2) / 2
+
+print("La media de número " + str(numero1) + " y el número " + str(numero2) + " es igual a: " + str(media))
